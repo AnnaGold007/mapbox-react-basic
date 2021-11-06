40e9f9519db6f627884948dbfa2972c9e039e206
AnnaGold007 <jorjik@gmail.com>
2021-11-06 15:47:42 +0200

    NavigationToolAdded

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import Geocoder from 'mapbox-gl-geocoder'
 import './Map.css';
+import 'mapbox-gl/dist/mapbox-gl.css'
+//import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import Tooltip from './components/Tooltip';
+import ReactDOM from 'react-dom';
 
 mapboxgl.accessToken =
-  'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+  'pk.eyJ1IjoiYW5uYWdvbGQwMDciLCJhIjoiY2t2bTNsajF2MWNiMDJ1dGtxM2lwOTZybSJ9.BvkgIPX-S7LrBwyqDMgiZw';
 
 const Map = () => {
   const mapContainerRef = useRef(null);
+  const tooltipRef = useRef(new mapboxgl.Popup({ offset: 15 }));
+
 
   const [lng, setLng] = useState(36.300);
   const [lat, setLat] = useState(31.400);
@@ -17,11 +24,19 @@ const Map = () => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
+    //  style: 'mapbox://styles/annagold007/ckvm8uwn90pnp15mgbl2bstt0',
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
+      logoPosition: 'bottom-left'
     });
 
-    // Add navigation control (the +/- zoom buttons)
+    map.addControl(
+      new Geocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+      }),'top-right'
+      );
+
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 
     map.on('move', () => {
@@ -29,7 +44,36 @@ const Map = () => {
       setLat(map.getCenter().lat.toFixed(4));
       setZoom(map.getZoom().toFixed(2));
     });
+//  // change cursor to pointer when user hovers over a clickable feature
+//     map.on('mouseenter', e => {
+//       if (e.features.length) {
+//         map.getCanvas().style.cursor = 'pointer';
+//       }
+//     });
 
+//     // reset cursor to default when user is no longer hovering over a clickable feature
+//     map.on('mouseleave', () => {
+//       map.getCanvas().style.cursor = '';
+//     });
+
+//     // add tooltip when users mouse move over a point
+//     map.on('mousemove', e => {
+//       const features = map.queryRenderedFeatures(e.point);
+//       if (features.length) {
+//         const feature = features[0];
+
+//         // Create tooltip node
+//         const tooltipNode = document.createElement('div');
+//         ReactDOM.render(<Tooltip feature={feature} />, tooltipNode);
+
+//         // Set tooltip on map
+//         tooltipRef.current
+//           .setLngLat(e.lngLat)
+//           .setDOMContent(tooltipNode)
+//           .addTo(map);
+//       }
+//     });
+  
     // Clean up on unmount
     return () => map.remove();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
